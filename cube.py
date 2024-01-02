9dcffccb2a1becae02a710a8b9a6f709dcc9af33
Logan <lot407@usask.ca>
2024-01-01 18:41:18 -0600

    cube representation mostly functional

--- cube.py
+++ cube.py
@@ -1,69 +1,8 @@
 from piece import Piece
 
-class Cube():
-    
-    
-     def selectFace(self,axis, direction):
-          side = []
-          if axis == "k":
-               for j in range(3):
-                    row = []
-                    for i in range(3):
-                         row.append(self.pieces[1 - direction][j][i])
-                    side.append(row)
-          elif axis == "j":
-               for k in range(3):
-                    row = []
-                    for i in range(3):
-                         row.append(self.pieces[k][1 - direction][i])
-                    side.append(row)
 
-          elif axis == "i":
-               for k in range(3):
-                    row = []
-                    for j in range(3):
-                         row.append(self.pieces[k][j][1 - direction])
-                    side.append(row)
+class Cube:
 
-          else:
-               print("Error selectFace")
-          return side
-
-     def setSide(self, face, sides, axis, direction):
-          for j in range(3):
-               for i in range(3):
-                    face[j][i].addSide(sides[j*3 + i], axis, direction)
-          return face
-     
-     def setFace(self, face, axis, direction):
-          if axis == "k":
-               for j in range(3):
-                    for i in range(3):
-                         self.pieces[1 - direction][j][i] = face[j][i]
-                    
-          elif axis == "j":
-               for k in range(3):
-                    for i in range(3):
-                         self.pieces[k][1-direction][i] = face[k][i]
-                    
-
-          elif axis == "i":
-               for k in range(3):
-                    for j in range(3):
-                         self.pieces[k][j][1-direction] = face[k][j]
-
-     def rotateFace(self, face, clockwise):
-          # print(face)
-          rotatedFace = []
-          for i in range(3):
-               row = []
-               for j in range(3):
-                    # face[j][i].rotate("k", True)
-                    row.append(face[j][i])
-               rotatedFace.append(row)
-          # print(rotatedFace)
-          return rotatedFace
- 
      def __init__(self, cube) -> None:
           self.pieces = []
           axis = "kkkkkkkkkjjjjjjjjjiiiiiiiiikkkkkkkkkjjjjjjjjjiiiiiiiii"
@@ -73,73 +12,299 @@ class Cube():
                     row = []
                     for j in range(3):
                          # print(k*9 + j*3 + i)
-                         row.append(Piece())  
+                         row.append(Piece())
                     threeby.append(row)
                self.pieces.append(threeby)
-               
+
           k = self.selectFace("k", 1)
           k = self.setSide(k, cube[0:9], "k", 1)
-          self.setFace(k, "k", 1)
+          # self.setFace(k, "k", 1)
 
           j = self.selectFace("j", 1)
           j = self.setSide(j, cube[9:18], "j", 1)
-          self.setFace(j, "j", 1)
+          # self.setFace(j, "j", 1)
 
           i = self.selectFace("i", 1)
           i = self.setSide(i, cube[18:27], "i", 1)
-          self.setFace(i, "i", 1)
+          # self.setFace(i, "i", 1)
 
           k = self.selectFace("k", -1)
           k = self.setSide(k, cube[27:36], "k", -1)
-          self.setFace(k, "k", -1)
+          # self.setFace(k, "k", -1)
 
           j = self.selectFace("j", -1)
           j = self.setSide(j, cube[36:45], "j", -1)
-          self.setFace(j, "j", -1)
+          # self.setFace(j, "j", -1)
 
           i = self.selectFace("i", -1)
           i = self.setSide(i, cube[45:54], "i", -1)
-          self.setFace(i, "i", -1)
+          # self.setFace(i, "i", -1)
 
 
-          piece = self.pieces[0][0][0]
-          print(piece.matrix)
+     def selectFace(self, axis, direction):
+          side = []
+          if axis == "k":
+               if direction == 1:
+                    for i in range(3):
+                         row = []
+                         for j in range(3):
+                              row.append(self.pieces[2][j][i])
+                         side.append(row)
+               else:
+                    for i in range(2, -1, -1):
+                         row = []
+                         for j in range(3):
+                              row.append(self.pieces[0][j][i])
+                         side.append(row)
+
+          elif axis == "j":
+               if direction == 1:
+                    for k in range(2, -1, -1):
+                         row = []
+                         for i in range(2, -1, -1):
+                              row.append(self.pieces[k][2][i])
+                         side.append(row)
+               else:
+                    for k in range(2, -1, -1):
+                         row = []
+                         for i in range(3):
+                              row.append(self.pieces[k][0][i])
+                         side.append(row)
 
 
-          f = self.selectFace("k", 1)
-          f = self.rotateFace(f, True)
-          self.setFace(f, "k", 1)
+          elif axis == "i":
+               if direction == 1:
+                    for k in range(2, -1, -1):
+                         row = []
+                         for j in range(3):
+                              row.append(self.pieces[k][j][2])
+                         side.append(row)
+               else:
+                    for k in range(2, -1, -1):
+                         row = []
+                         for j in range(2, -1, -1):
+                              row.append(self.pieces[k][j][0])
+                         side.append(row)
+
+          else:
+               print("Error selectFace")
+          return side
 
 
-          piece = self.pieces[0][0][0]
-          print(piece.matrix)
+     def setSide(self, face, sides, axis, direction):
+          for j in range(3):
+               for i in range(3):
+                    face[j][i].addSide(sides[j * 3 + i], axis, direction)
+          return face
+
+
+     def setFace(self, face, axis, direction):
+          if axis == "k":
+               if direction == 1:
+                    for i in range(3):
+                         for j in range(3):
+                              self.pieces[2][j][i] = face[i][j]
+               else:
+                    for i in range(2, -1, -1):
+                         for j in range(3):
+                              self.pieces[0][j][i] = face[2-i][j]
+
+          elif axis == "j":
+               if direction == 1:
+                    for k in range(2, -1, -1):
+                         for i in range(2, -1, -1):
+                              self.pieces[k][2][i] = face[2-k][2-i]
+               else:
+                    for k in range(2, -1, -1):
+                         for i in range(3):
+                              self.pieces[k][0][i] = face[2-k][i]
+
+
+          elif axis == "i":
+               if direction == 1:
+                    for k in range(2, -1, -1):
+                         for j in range(3):
+                              self.pieces[k][j][2] = face[2-k][j]
+               else:
+                    for k in range(2, -1, -1):
+                         for j in range(2, -1, -1):
+                              self.pieces[k][j][0] = face[2-k][2-j]
+
+
+     def rotateFace(self, axis, direction, clockwise):
           
+          face = self.selectFace(axis, direction)
+          rotatedFace = []
 
-          # k_ = self.selectFace("k", 1)
-          # for i in range(3):
-          #      for j in range(3):
-          #           print(k_[i][j].matrix)
-          # z = 0
-          # for t, direction in enumerate([1, -1], start= 1):
-          #      print(direction, z)      
-               
-          #      for j in range(3):
-          #           for i in range(3):
-          #                pieces[k][j][i].addSide(cube[z], axis[z], direction)
-          #                print(pieces[k][j][i].matrix)
-          #                z += 1
-                    
-          # for matrix in self.pieces:
-          #      for row in matrix:
-          #           for piece in row:
-          #                print(piece.matrix)
-          # print(pieces[0][0][1].matrix)
-          # pieces[1][1][1].rotate("k", True)
-          # print(pieces[1][1][1].matrix)
-          # pieces[1][1][1].rotate("k", False)
-          # print(pieces[1][1][1].matrix)
+          if (clockwise and direction == 1) or (not clockwise and direction == -1 ):
+               for i in range(3 ):
+                    row = []
+                    # column.append([face[0][i], face[1][i], face[2][i]])
+                    for j in range(2, -1, -1):
+                         face[j][i].rotate(axis, clockwise)
+                         row.append(face[j][i])
+                         
+                    rotatedFace.append(row)
+          else:
+               for i in range(2, -1, -1 ):
+                    row = []
+                    # column.append([face[0][i], face[1][i], face[2][i]])
+                    for j in range(3):
+                         # print(face[i][j].matrix)
+                         face[j][i].rotate(axis, clockwise)
+                         row.append(face[j][i])
 
-          
+                    rotatedFace.append(row)
                 
+          self.setFace(rotatedFace, axis, direction)
 
-cube = Cube("wwwwwwwwwrrrrrrrrrgggggggggyyyyyyyyyooooooooobbbbbbbbb")
+     def move(self, moves):
+          for move in moves:
+               if move == "u":
+                    self.rotateFace("k", 1, True)
+               elif move == "U":
+                    self.rotateFace("k", 1, False)
+               elif move == "r":
+                    self.rotateFace("j", 1, True)
+               elif move == "R":
+                    self.rotateFace("j", 1, False)
+               elif move == "f":
+                    self.rotateFace("i", 1, True)
+               elif move == "F":
+                    self.rotateFace("i", 1, False)
+               elif move == "d":
+                    self.rotateFace("k", -1, True)
+               elif move == "D":
+                    self.rotateFace("k", -1, False)
+               elif move == "l":
+                    self.rotateFace("j", -1, True)
+               elif move == "L":
+                    self.rotateFace("j", -1, False)
+               elif move == "b":
+                    self.rotateFace("i", -1, True)
+               elif move == "B":
+                    self.rotateFace("i", -1, False)
+               
+     def stringRepresentation(self) -> str:
+          string = ""
+
+          u = self.selectFace("k", 1)
+          for row in u:
+               for entry in row:
+                    string += str(entry.selectSide("k"))[-1]
+
+          r = self.selectFace("j", 1)
+          for row in r:
+               for entry in row:
+                    string += str(entry.selectSide("j"))[-1]
+
+          f = self.selectFace("i", 1)
+          for row in f:
+               for entry in row:
+                    string += str(entry.selectSide("i"))[-1]
+
+          d = self.selectFace("k", -1)
+          for row in d:
+               for entry in row:
+                    string += str(entry.selectSide("k"))[-1]
+
+          l = self.selectFace("j", -1)
+          for row in l:
+               for entry in row:
+                    string += str(entry.selectSide("j"))[-1]
+
+          b = self.selectFace("i", -1)
+          for row in b:
+               for entry in row:
+                    string += str(entry.selectSide("i"))[-1]
+          return string
+
+     def __str__(self) -> str:
+          string = ""
+          i = 0
+          sides = self.stringRepresentation()
+          # print(sides)
+          u = sides[0:9]
+          for j in range(3):
+               string += "        "
+               for i in range(3):
+                    string += u[j * 3 + i] + " "
+               string += "\n"
+
+          l = sides[36:45]
+          f = sides[18:27]
+          r = sides[9:18]
+          b = sides[45:54]
+
+          for j in range(3):
+               string += " "
+               for i in range(3):
+                    string += l[j * 3 + i] + " "
+
+               string += " "
+               for i in range(3):
+                    string += f[j * 3 + i] + " "
+
+               string += " "
+               for i in range(3):
+                    string += r[j * 3 + i] + " "
+
+               string += " "
+               for i in range(3):
+                    string += b[j * 3 + i] + " "
+
+               string += "\n"
+
+          d = sides[27:36]
+          for j in range(3):
+               string += "        "
+               for i in range(3):
+                    string += d[j * 3 + i] + " "
+               string += "\n"
+
+          return string
+# cube = Cube("wwwwwwwwwrrrrrrrrrgggggggggyyyyyyyyyooooooooobbbbbbbbb")
+# cube = Cube("123456789123rrrrrr456ggggggyyyyyyyyyooooooooobbbbbbbbb")
+# cube = Cube("123456789123456789123456789123456789123456789123456789")
+cube = Cube("wwrwwyooygrbrrbywywgryggbrbwbroyogoorrggogoyowbgybwbby")
+
+"rufubdlrfbdurrlfb"
+print(cube)
+cube.move("BFLRRUDBFRLDBUFUR")
+print(cube)
+# print("\nkp")
+# cube.rotateFace("k", -1, True)
+# print(cube)
+# cube.rotateFace("k", -1, False)
+# print(cube)
+
+# print("\nj")
+# cube.rotateFace("j", 1, True)
+# print(cube)
+# cube.rotateFace("j", 1, False)
+# print(cube)
+
+# print("\njp")
+# cube.rotateFace("j", -1, True)
+# print(cube)
+# cube.rotateFace("j", -1, False)
+# print(cube)
+
+# print("\ni")
+# cube.rotateFace("i", 1, True)
+# print(cube)
+# cube.rotateFace("i", 1, False)
+# print(cube) 
+
+# print("\nip")
+# cube.rotateFace("i", -1, True)
+# print(cube)
+# cube.rotateFace("i", -1, False)
+# print(cube)
+
+# for k in range(3):
+#      for j in range(3):
+#           for i in range(3):
+#                print(k,j,i)
+#                print(cube.pieces[k][j][i])
+
--- piece.py
+++ piece.py
@@ -1,104 +1,335 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Colour:
     colour: str
     value: int
+
     def __mul__(self, y):
-        
         if self.value * y == 0:
             return 0
         self.value = self.value * y
         return self
-    
+
     def __rmul__(self, y):
-        
         if self.value * y == 0:
             return 0
         self.value = self.value * y
         return self
-    
+
     def __add__(self, y):
         self.value += y
         return self
-    
+
     def __radd__(self, y):
         self.value += y
 
         return self
-    
+
     def __str__(self):
         if self.value > 0:
             return self.colour
         else:
             return "-" + self.colour
 
-class Piece():
 
+class Piece:
     def __init__(self) -> None:
-        self.matrix = [] #Row vectors
-    
+        self.matrix = []  # Column vectors
+
     def addSide(self, colour, axis, direction):
         if axis == "k":
-            self.matrix.append([0,0,Colour(colour, direction)])
+            self.matrix.append([0, 0, Colour(colour, direction)])
         elif axis == "j":
             self.matrix.append([0, Colour(colour, direction), 0])
         elif axis == "i":
             self.matrix.append([Colour(colour, direction), 0, 0])
-        
+
         else:
-            print("error")    
+            print("error")
         # print(self.matrix)
 
     def rotate(self, axis, clockwise):
-        new_matrix = [[0,0,0], [0,0,0], [0,0,0]]
-        kcw = [[0,1,0], [-1,0,0], [0,0,1]] #Column vectors
-        kccw = [[0,-1,0], [1,0,0], [0,0,1]]
-        if axis == 'k':
+        new_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+        # Rotation matrices (column wise)
+        kcw = [[0, 1, 0], [-1, 0, 0], [0, 0, 1]]  
+        kccw = [[0, -1, 0], [1, 0, 0], [0, 0, 1]]
+        jcw = [[0,0,1], [0,1,0], [-1,0,0]]
+        jccw = [[0,0,-1], [0,1,0], [1,0,0]]
+        iccw = [[1,0,0], [0,0,-1], [0,1,0]]
+        icw = [[1,0,0], [0,0,1], [0,-1,0]]
+
+        def matrixMult(self, matrix):
+            for i in range(len(self.matrix)):
+                    for j in range(3):
+                        new_matrix[i][j] = (
+                            self.matrix[i][0] * matrix[j][0]
+                            + self.matrix[i][1] * matrix[j][1]
+                            + self.matrix[i][2] * matrix[j][2]
+                        )
+            self.matrix = new_matrix
+
+
+        if axis == "k":
             if clockwise:
-                for i in range(3):
-                    for j in range(3):
-                        new_matrix[i][j] = self.matrix[i][0] * kcw[j][0] + self.matrix[i][1] * kcw[j][1] + self.matrix[i][2] * kcw[j][2]
+                matrixMult(self, kcw)
             else:
-                for i in range(3):
-                    for j in range(3):
-                        new_matrix[i][j] = self.matrix[i][0] * kccw[j][0] + self.matrix[i][1] * kccw[j][1] + self.matrix[i][2] * kccw[j][2]
-                
-                
-        self.matrix = new_matrix
+                matrixMult(self, kccw)
+        elif axis == "j":
+            if clockwise:
+                matrixMult(self, jcw)
+            else:
+                matrixMult(self, jccw)
+        elif axis == "i":
+            if clockwise:
+                matrixMult(self, icw)
+            else:
+                matrixMult(self, iccw)
+        else:
+            raise ValueError
+
+        # if axis == "k":
+        #     if clockwise:
+        #         for i in range(len(self.matrix)):
+        #             for j in range(3):
+        #                 new_matrix[i][j] = (
+        #                     self.matrix[i][0] * kcw[j][0]
+        #                     + self.matrix[i][1] * kcw[j][1]
+        #                     + self.matrix[i][2] * kcw[j][2]
+        #                 )
+            
+        #     else:
+        #         for i in range(len(self.matrix)):
+        #             for j in range(3):
+        #                 new_matrix[i][j] = (
+        #                     self.matrix[i][0] * kccw[j][0]
+        #                     + self.matrix[i][1] * kccw[j][1]
+        #                     + self.matrix[i][2] * kccw[j][2]
+        #                 )
+    
 
 
+        # self.matrix = new_matrix
 
+    def selectSide(self, axis):
+        side = 0
+        if axis == "k":
+            for column in self.matrix:
+                side += column[2] * 1
+        elif axis == "j":
+            for column in self.matrix:
+                side += column[1] * 1
+        elif axis == "i":
+            for column in self.matrix:
+                side += column[0] * 1
+        else:
+            raise ValueError
+
+        return side
+    
+    def getSide(self, axis):
+        testValue = 0
+        if axis == "k":
+            for i in range(len(self.matrix)):
+                testValue += self.matrix[i][2] * 1
+        elif axis == "j":
+            for i in range(len(self.matrix)):
+                testValue += self.matrix[i][1] * 1
+        else:
+            for i in range(len(self.matrix)):
+                testValue += self.matrix[i][0] * 1
+        return testValue
 
     def __str__(self) -> str:
-        string =""
-        for x in self.matrix:
-            string += str(x[0]) + " " +  str(x[1])+ " " + str(x[2]) + "\n"
+        string = ""
+        for i in range(3):
+            for j in range(len(self.matrix)):
+                string += str(self.matrix[j][i]) + " "
+            string += "\n"
         return string
 
 
-# red = Colour('r', 1)
-# blue = Colour('b', 1)
-# white = Colour('w', 1)
+# """Test centre piece"""
 
-# piece = Piece(white, red, blue)
+# piece = Piece()
 
-# print(piece)
+# piece.addSide("w", "k", 1)
+
+# # Test 1
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w":
+#     print("Error test 1")
+
+# # Test 2    
+# piece.rotate("k", True)
+
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w":
+#     print("Error test 2")
+
+# # Test 3
+# piece.rotate("k", True)
+# piece.rotate("k", True)
+
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w":
+#     print("Error test 3")
+
+
+# """Test corner piece"""
+
+# piece = Piece()
+
+# piece.addSide("w", "k", 1)
+# piece.addSide("o", "j", -1)
+# piece.addSide("g", "i", 1)
+
+# piece.rotate("k", True)
+# piece.rotate("k", False)
+
+# # Test 4
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][0] * 1
+
+# if testValue.colour != "g" or testValue.value != 1:
+#     print("Error test 4")
+
+# # Test 5
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][1] * 1
+
+# if testValue.colour != "o" or testValue.value != -1:
+#     print("Error test 5")
+
+# # Test 6
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w" or testValue.value != 1:
+#     print("Error test 6")
+
+# piece.rotate("k", True)
+
+# # Test 7
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][0] * 1
+
+# if testValue.colour != "o" or testValue.value != -1:
+#     print("Error test 7")
+
+# # Test 8
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][1] * 1
+
+# if testValue.colour != "g" or testValue.value != -1:
+#     print("Error test 8")
+
+# # Test 9
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w" or testValue.value != 1:
+#     print("Error test 9")
 
 # piece.rotate("k", False)
-# print(piece)
-# piece.rotate("k", True)
-# print(piece)
-# piece.rotate("k", False)
-# print(piece)
-# piece.rotate("k", True)
-# print(piece)
+# piece.rotate("j", True)
+# piece.rotate("j", False)
+# piece.rotate("i", True)
+# piece.rotate("i", False)
 
-# piece.rotate("k", True)
-# print(piece)
-# piece.rotate("k", True)
-# print(piece)
-# piece.rotate("k", False)
-# print(piece)
-# piece.rotate("k", False)
-# print(piece)
+
+# # Test 10
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][0] * 1
+
+# if testValue.colour != "g" or testValue.value != 1:
+#     print("Error test 10")
+
+# # Test 11
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][1] * 1
+
+# if testValue.colour != "o" or testValue.value != -1:
+#     print("Error test 11")
+
+# # Test 12
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "w" or testValue.value != 1:
+#     print("Error test 12")
+
+
+# piece.rotate("j", True)
+# # Test 13
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][0] * 1
+
+# if testValue.colour != "w" or testValue.value != 1:
+#     print("Error test 13")
+
+# # Test 14
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][1] * 1
+
+# if testValue.colour != "o" or testValue.value != -1:
+#     print("Error test 14")
+
+# # Test 15
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "g" or testValue.value != -1:
+#     print("Error test 15")
+
+# piece.rotate("j", False)
+# piece.rotate("i", True)
+
+# # Test 16
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][0] * 1
+
+# if testValue.colour != "g" or testValue.value != 1:
+#     print("Error test 16")
+
+# # Test 17
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][1] * 1
+
+# if testValue.colour != "w" or testValue.value != 1:
+#     print("Error test 17")
+
+# # Test 18
+# testValue = 0
+# for i in range(len(piece.matrix)):
+#     testValue += piece.matrix[i][2] * 1
+
+# if testValue.colour != "o" or testValue.value != 1:
+#     print("Error test 18")
+
+
